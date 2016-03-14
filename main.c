b2f594161cb911082132319816ac5993a61c7340
Wagner Alves <wagner.ferreira.alves@usp.br>
2016-03-14 16:33:52 -0300

    Primeiro commit do main

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int m, n, r, s;
+	m = atoi(argv[1]);
+	n = atoi(argv[2]);
+	r = atoi(argv[3]);
+	s = atoi(argv[4]);
+	printf("%d %d %d %d\n", m, n, r, s);
+
+	return 0;
+}
