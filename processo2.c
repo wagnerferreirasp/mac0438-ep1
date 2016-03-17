9df091516da8a9559cae4fb0619a7b1dd9a0dc94
Rafael Verona <raverona@gmail.com>
2016-03-17 12:29:51 -0300

    Criado o processo 2

---
+++ processo2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long calcula_fibonacci(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	if (n == 2) return 1;
+	return calcula_fibonacci(n-1) + calcula_fibonacci(n-2);
+}
+
+int fibonacci(int n) {
+	printf("[fibonacci] Calculando o %d-ésimo número de Fibonacci\n", n);
+	printf("[fibonacci] O %d-ésimo número de Fibonacci é: %lld\n", n, fibonacci(n));
+    return 0;
+}
